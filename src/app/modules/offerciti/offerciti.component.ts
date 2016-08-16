9ee8f1139e73d196201a5178fbc7764fff44e6f2
santosh <santosh.gudikandula@adap.tv>
2016-08-16 17:31:10 +0530

    Angular components structure

---
+++ src/app/modules/offerciti/offerciti.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'offerciti',
+	templateUrl: 'app/modules/offerciti/offerciti.component.html',
+	styleUrls: [ 'app/modules/offerciti/offerciti.component.css']
+})
+export class OffercitiComponent {}
