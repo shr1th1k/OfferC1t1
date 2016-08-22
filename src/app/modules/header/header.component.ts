9bd9ab301cb30dbc141408299a0474948777ca32
santosh <santosh.gudikandula@adap.tv>
2016-08-22 23:44:13 +0530

    Coverted css to scss format and done configurations

---
+++ src/app/modules/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+	selector: 'offerciti-header',
+	templateUrl: 'app/modules/header/header.component.html',
+	styleUrls: [ 'app/modules/header/header.component.css']
+})
+export class HeaderComponent {}
