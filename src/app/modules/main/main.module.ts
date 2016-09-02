8a9abd09b107c93326d54b31cf59a48c5b8fe966
santosh <santosh.gudikandula@adap.tv>
2016-09-02 15:48:01 +0530

    Declaration component is added to main module

--- src/app/modules/main/main.module.ts
+++ src/app/modules/main/main.module.ts
@@ -2,12 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { OffercitiComponent } from '../offerciti/offerciti.component';
+import { LoginComponent } from '../../../app/modules/login/login.component';
 import { routing } from '../../shared/modules/route.component';
 
 
 @NgModule({
     imports: [ BrowserModule, routing ],
-    declarations: [ OffercitiComponent ],
+    declarations: [ OffercitiComponent, LoginComponent ],
     bootstrap: [ OffercitiComponent ]
 })
 export class OffercitiModule {}
